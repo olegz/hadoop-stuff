6e96a1af0569e0cead161a90900068d904f4c661
Oleg Zhurakousky <oleg@Olegs-MacBook-Pro.local>
2012-12-25 08:44:27 -0500

    pushing

--- src/main/java/oz/poc/main/CompressedSequenceFile.java
+++ src/main/java/oz/poc/main/CompressedSequenceFile.java
@@ -10,6 +10,7 @@ public class CompressedSequenceFile {
 	public static void main(String[] args) {
 		System.out.println("######## Starting task #########");
 		System.out.println("Arguments: " + Arrays.asList(args) + " " + args.length);
+		System.out.println("System property: " + System.getProperty("setup"));
 		System.out.println("Hello");
 	}
 
